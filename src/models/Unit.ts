2052d672a0bcf9732bb2060395712a8ad31e49b0
bapt-man <baptiste.boin@iia-formation.fr>
2026-03-09 23:10:31 +0100

    update class Unit var

--- src/models/Unit.ts
+++ src/models/Unit.ts
@@ -5,7 +5,7 @@ import { Position } from "./Types"
 export class Unit {
   id: string;        // Unique ID de l'unité
   name: string;      // "Marth", "Ike"...
-  ownerId: number;   // À quel joueur appartient ce pion
+  ownerGameId: number;   // À quel joueur appartient ce pion
 
   position: Position;
   
@@ -19,10 +19,10 @@ export class Unit {
   
   hasMoved: boolean; // Pour savoir s'il a déjà joué ce tour
 
-  constructor(id: string, name: string, ownerId: number, x: number, y: number, z: number) {
+  constructor(id: string, name: string, ownerGameId: number, x: number, y: number, z: number) {
     this.id = id;
     this.name = name;
-    this.ownerId = ownerId;
+    this.ownerGameId = ownerGameId;
 
     this.position = { x, y, z };
 
